f0da22958ff560c6288fa3bea18a6cc7b9ae7b49
Kulik Nikita <kulik.nik@gmail.com>
2023-07-06 07:04:38 +0300

    Complete 01, 02, 03 tasks from third lessons

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -2,14 +2,34 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
+  _accum = '';
+  arrStr = [];
   constructor(options) {
     super(options);
   }
 
-  _transform(chunk, encoding, callback) {
+  get accum() {
+    return this._accum;
   }
 
+  set accum(value) {
+    this._accum  = value;
+  }
+
+  _transform(chunk, encoding, callback) {
+    this.accum += chunk.toString()
+    if (this.accum.includes(os.EOL)) {
+      const [first, second] = this.accum.split(os.EOL);
+      this.push(first);
+      this.accum = second;
+      }
+      callback(null, null);
+    }
+
   _flush(callback) {
+    this.push(this.accum);
+    this.accum = '';
+    callback();
   }
 }
 
--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -9,16 +10,41 @@ server.on('request', (req, res) => {
   const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
+  let stream = null;
 
   switch (req.method) {
     case 'GET':
+      if (pathname.split('/').length > 1) {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.end('Ошибка: неверный путь до файла')
+      } else {
+        stream = fs.createReadStream(filepath);
+        stream.on('error', (err)=> {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html; charset=utf-8');
+          res.end('Ошибка:  файла на диске нет');
+        });
+        stream.on('open', ()=> {
+          res.statusCode = 200;
+          stream.pipe(res);
+        });
+      }
 
       break;
 
     default:
       res.statusCode = 501;
+       res.setHeader('Content-Type', 'text/html; charset=utf-8');
       res.end('Not implemented');
   }
+
+
+  req.on('aborted', ()=> {
+    if(stream) {
+      stream.destroy();
+    }
+  })
 });
 
 module.exports = server;
