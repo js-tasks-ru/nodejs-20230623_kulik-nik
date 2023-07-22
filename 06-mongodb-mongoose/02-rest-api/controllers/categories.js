e404d92fcf518c584571900058c5e94732fba87a
kulik nikita <kulik.nik@gmail.com>
2023-07-22 22:43:15 +0300

    complete second task in 5th lesson

--- 06-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,11 @@
+const mapCategory = require('../mappers/category');
+const modelCategory = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = [];
+  (await modelCategory.find()).forEach((category) => categories.push(mapCategory(category)));
+  ctx.response.status = 200;
+  ctx.body = {
+    categories,
+  };
 };
--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,60 @@
+const {
+  mongoose,
+  ObjectId,
+} = require('mongoose');
+const mapProduct = require('../mappers/product');
+const modelProduct = require('../models/Product');
+
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  const {subcategory} = ctx.query;
+  const {
+    subcategory,
+  } = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  if (mongoose.isValidObjectId(subcategory)) {
+    const products = [];
+    (await modelProduct.find({
+      'subcategory': subcategory,
+    })).forEach((product) => products.push(mapProduct(product)));
+
+    ctx.status = 200;
+    ctx.type = 'application/json';
+    ctx.body = {
+      products,
+    };
+  } else {
+    ctx.throw(400, 'Not Found');
+  }
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = [];
+  (await modelProduct.find()).forEach((product) => products
+    .push(mapProduct(product)));
+  ctx.status = 200;
+  ctx.type = 'application/json';
+  ctx.body = {
+    products,
+  };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const {
+    id,
+  } = ctx.params;
+  if (mongoose.isValidObjectId(id)) {
+    const product = await modelProduct.findById(id);
+    if (!product) {
+      ctx.throw(404, 'No product found');
+    }
+    ctx.status = 200;
+    ctx.type = 'application/json';
+    ctx.body = {
+      product: mapProduct(product),
+    };
+  } else {
+    ctx.throw(400, 'Not Found');
+  }
 };
-
